6d8c6314346e8b679913af02d897aeb717b0e78d
Ann Mercin K S <ann.mercin@zot.app>
2025-10-28 16:09:26 +0530

    first commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavBar } from './shared/nav-bar/nav-bar';
+import { SideNav } from './shared/side-nav/side-nav';
+import { Footer } from './shared/footer/footer';
+import { MatIcon } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    NavBar,
+    SideNav,
+    Footer,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    MatIcon
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
